d339eb1ea2c8d355506d3ae8afb6c8684b1adee7
Renja-Nanzuu <ramadhananan001@gmail.com>
2026-05-31 11:02:31 +0700

    Update kodingan UAS

--- UAS-StrukDat-Main.cpp
+++ UAS-StrukDat-Main.cpp
@@ -12,6 +12,174 @@ int jumlahAkun = 4;
 // Untuk Menampung Username yang sedang Login saat ini
 string userAktif = ""; // nanti akan berubah saat berhasil login!
 
+// Nampung Data Stasiun
+struct Stasiun {
+    string kode;
+    string nama;
+};
+
+Stasiun stasiun[20];
+int jumlahStasiun = 0;
+
+void initStasiun() {
+    stasiun[0] = {"GMR", "Gambir"};
+    stasiun[1] = {"BKS", "Bekasi"};
+    stasiun[2] = {"KW", "Karawang"};
+    stasiun[3] = {"CRB", "Cirebon"};
+    stasiun[4] = {"PWT", "Purwokerto"};
+    stasiun[5] = {"YK", "Yogyakarta"};
+    stasiun[6] = {"SLO", "Solo Balapan"};
+    stasiun[7] = {"SMT", "Semarang Tawang"};
+    stasiun[8] = {"SPT", "Surabaya Pasar Turi"};
+
+    jumlahStasiun = 9;
+}
+
+// Nampung Data Jenis Kereta Api
+struct Kereta {
+    string kode;
+    string namaKereta;
+    int rute[20];
+    int jumlahRute;
+    int kursiTersedia;
+};
+
+Kereta kereta[10];
+int jumlahKereta = 3;
+
+void initKereta() {
+    // KA Anggrek
+    kereta[0].kode = "KA001";
+    kereta[0].namaKereta = "KA Anggrek";
+
+    kereta[0].rute[0] = 0; // Gambir
+    kereta[0].rute[1] = 1; // Bekasi
+    kereta[0].rute[2] = 2; // Karawang
+    kereta[0].rute[3] = 3; // Cirebon
+    kereta[0].rute[4] = 7; // Semarang Tawang
+    kereta[0].rute[5] = 8; // Surabaya Pasar Turi
+    kereta[0].jumlahRute = 6;
+
+    // KA Taksaka
+    kereta[1].kode = "KA002";
+    kereta[1].namaKereta = "KA Taksaka";
+
+    kereta[1].rute[0] = 0; // Gambir
+    kereta[1].rute[1] = 1; // Bekasi
+    kereta[1].rute[2] = 2; // Karawang
+    kereta[1].rute[3] = 3; // Cirebon
+    kereta[1].rute[4] = 4; // Purwokerto
+    kereta[1].rute[5] = 5; // Yogyakarta
+    kereta[1].jumlahRute = 6;
+
+    // KA Fajar Utama Solo
+    kereta[2].kode = "KA003";
+    kereta[2].namaKereta = "KA Fajar Utama Solo";
+
+    kereta[2].rute[0] = 0; // Gambir
+    kereta[2].rute[1] = 1; // Bekasi
+    kereta[2].rute[2] = 2; // Karawang
+    kereta[2].rute[3] = 3; // Cirebon
+    kereta[2].rute[4] = 4; // Purwokerto
+    kereta[2].rute[5] = 6; // Solo Balapan
+    kereta[2].jumlahRute = 6;
+}
+
+// Nampung Data Jadwal
+struct Jadwal {
+    string kodeJadwal;
+    int idKereta;
+    int jamBerangkat;
+    int menitBerangkat;
+    int jamTiba;
+    int menitTiba;
+    int kursiTersedia;
+    bool arahPulang;
+};
+
+Jadwal jadwal[20];
+int jumlahJadwal = 0;
+
+void initJadwal() {
+    jadwal[0] = {
+        "JD001",
+        0,
+        8,0,
+        17,30,
+        300,
+        false
+    };
+    jadwal[1] = {
+        "JD002",
+        0,
+        20,0,
+        5,30,
+        300,
+        true
+    };
+    jadwal[2] = {
+        "JD003",
+        1,
+        9,0,
+        15,30,
+        250,
+        false
+    };
+    jadwal[3] = {
+        "JD004",
+        2,
+        6,0,
+        14,0,
+        250,
+        false
+    };
+    jumlahJadwal = 4;
+}
+
+void OutputJadwal() {
+    initKereta();
+    initJadwal();
+    for(int i=0; i<jumlahJadwal; i++) {
+        cout << i+1 << ". "
+             << kereta[jadwal[i].idKereta].namaKereta
+             << " | ";
+
+        if(jadwal[i].jamBerangkat < 10)
+            cout << "0";
+
+        cout << jadwal[i].jamBerangkat << ":";
+
+        if(jadwal[i].menitBerangkat < 10)
+            cout << "0";
+
+        cout << jadwal[i].menitBerangkat;
+        cout << endl;
+    }
+
+}
+
+void tampilStasiun() {
+    initStasiun();
+    cout << string(30, '=') << endl;
+    CenterText("DAFTAR STASIUN",30);
+    cout << string(30, '=') << endl;
+    for(int i=0; i<jumlahStasiun; i++) {
+        cout << i+1
+             << ". "
+             << stasiun[i].nama
+             << endl;
+    }
+}
+
+void pilihRute(int &asal, int &tujuan) {
+    cout << "\nPilih Stasiun Asal : ";
+    cin >> asal;
+    cout << "Pilih Stasiun Tujuan : ";
+    cin >> tujuan;
+    asal--;
+    tujuan--;
+}
+
 void CenterText(string Teks, int width) {
     int padding = (width - Teks.length()) / 2;
     cout << string(padding, ' ') << Teks << endl;
@@ -63,17 +231,11 @@ bool login() {
     return false;
 }
 
-void Tampilanmenu() {
-    string pesan = "Selamat Datang, " + userAktif;
-    cout << string(70, '=') << endl;
-    CenterText(pesan,70);
-    CenterText("Silahkan pilih Fitur yang ingin digunakan",70);
-    cout << string(70, '=') << endl;
+void TampilanMenu() {
     cout << "1. Beli Tiket" << endl;
     cout << "2. Cek Tiket" << endl;
     cout << "3. Batalkan Tiket" << endl;
     cout << "4. Keluar" << endl;
-    cout << "Pilih menu : ";
 }
 
 int main() {
@@ -121,99 +283,58 @@ int main() {
                 system("cls");
                 break;
             default:
-                cout << "Gak ada pilihannya bang!" << endl;
+                cout << "Pilihan Tidak ada!" << endl;
                 pilihLogin = 0;
-                break;
+                cout << "Tekankan Enter untuk input ulang:";
+                cin.ignore();
+                cin.get();
+                system("cls");
         }
     }
 
-    Tampilanmenu();
+    int pilihMenu;
+    bool Selesai = false;
 
-//     string username, password;
+    while (!Selesai) {
+        string pesan = "Selamat Datang, " + userAktif;
+        cout << string(70, '=') << endl;
+        CenterText(pesan,70);
+        CenterText("Silahkan pilih Fitur yang ingin digunakan",70);
+        cout << string(70, '=') << endl;
+        TampilanMenu();
+        cout << "Pilih Fitur (1-4): ";
+        cin >> pilihMenu;
+        switch (pilihMenu) {
+            case 1:
+                system("cls");
+                int asal;
+                int tujuan;
 
-//     // data akun
-//     string user[10];
-//     string pass[10];
-
-//     // akun awal
-//     user[0] = "ridho";
-//     pass[0] = "ridho005";
-
-//     user[1] = "anandya";
-//     pass[1] = "anandya026";
-
-//     user[2] = "sri";
-//     pass[2] = "sri014";
-
-//     user[3] = "wildan";
-//     pass[3] = "wildan069";
-
-//     int jumlahAkun = 4;
-
-//     int pilihAwal;
-
-//     cout << "======================================" << endl;
-//     cout << "   SISTEM PEMESANAN TIKET KERETA" << endl;
-//     cout << "======================================" << endl;
-
-//     cout << "\n1. Login" << endl;
-//     cout << "2. Registrasi" << endl;
-//     cout << "Pilih : ";
-//     cin >> pilihAwal;
-
-//     // REGISTRASI
-//     if (pilihAwal == 2) {
-
-//         cout << "\n===== REGISTRASI =====" << endl;
-
-//         cout << "Buat Username : ";
-//         cin >> user[jumlahAkun];
-
-//         cout << "Buat Password : ";
-//         cin >> pass[jumlahAkun];
-
-//         jumlahAkun++;
-
-//         cout << "\nRegistrasi berhasil!" << endl;
-//     }
-
-//     // LOGIN
-//     bool login = false;
-
-//     cout << "\n===== LOGIN =====" << endl;
-
-//     cout << "Username : ";
-//     cin >> username;
-
-//     cout << "Password : ";
-//     cin >> password;
-
-//     for (int i = 0; i < jumlahAkun; i++) {
-
-//         if (username == user[i] && password == pass[i]) {
-//             login = true;
-//         }
-//     }
-
-//     if (login == false) {
-//         cout << "\nLogin gagal!" << endl;
-//         return 0;
-//     }
-
-//     cout << "\nLogin berhasil!1" << endl;
-
-//     int menu;
-
-//     do {
-
-//         cout << "\n========== MENU UTAMA ==========" << endl;
-//         cout << "1. Beli Tiket" << endl;
-//         cout << "2. Cek Tiket" << endl;
-//         cout << "3. Batalkan Tiket" << endl;
-//         cout << "4. Keluar" << endl;
-//         cout << "Pilih menu : ";
-//         cin >> menu;
-
+                tampilStasiun();
+                //pilihRute(asal, tujuan);
+                cout << "Tekankan Enter untuk input ulang:";
+                cin.ignore();
+                cin.get();
+                system("cls");
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                cout << string(70, '=') << endl;
+                cout << "Terima Kasih sudah menggunakan Mini APP KAI ACCESS!\nSampai Jumpa Kembali!" << endl;
+                Selesai = true;
+                break;
+            default:
+                cout << "Pilihan Tidak ada!" << endl;
+                pilihMenu = 0;
+                cout << "Tekankan Enter untuk input ulang:";
+                cin.ignore();
+                cin.get();
+                system("cls");
+        }
+    }
 //         // BELI TIKET
 //         if (menu == 1) {
 
